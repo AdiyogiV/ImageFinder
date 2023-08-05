11c06cee51fe3479b72627c36a00c819af1dec9a
Abhinav Vashisht <abhinav.vashisht98@gmail.com>
2023-08-05 22:26:19 +0530

    Update init.py

--- init.py
+++ init.py
@@ -3,7 +3,8 @@ from image_preprocessing import store_image_features
 
 
 if __name__ == "__main__":
-    clusters = 10
+    clusters = 12
     images_to_download = 1000
     download_images(images_to_download)
-    store_image_features(10)
+    store_image_features(clusters)
+    print("Project has been initialized. Execute app.py to run.")
