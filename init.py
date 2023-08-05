940fec0b153d1403eef7d3f2c360511ecdccd929
Abhinav Vashisht <abhinav.vashisht98@gmail.com>
2023-08-05 18:27:01 +0530

    Update init.py

--- init.py
+++ init.py
@@ -5,5 +5,5 @@ from image_preprocessing import store_image_features
 if __name__ == "__main__":
     clusters = 10
     images_to_download = 1000
-    #download_images(images_to_download)
+    download_images(images_to_download)
     store_image_features(10)
